93480eebb434adca04984b1a9f6b33efa98b7238
Muga Yoshikawa <muga@Mugas-MacBook-Air.local>
2017-11-25 13:12:34 +0900

    added another album 'Italy 2015 winter' and introduced 'row' tag in the HTML file.

--- js/album_preview.js
+++ js/album_preview.js
@@ -1,7 +1,7 @@
 (function () {
 	'use strict'
 	var prevIndex = 0;
-	var albums = ['italy1', 'spain', 'uk']
+	var albums = ['italy1', 'italy2', 'spain', 'uk']
 	var i;
 	for (i = 0; i < albums.length; i++) {
 		showPreviews(albums[i], prevIndex);
@@ -13,10 +13,6 @@
 		for (i = 0; i < slides.length; i++) {
 			slides[i].style.display = "none";
 		}
-		// prevIndex++;
-		// if (prevIndex > slides.length) {
-		// 	prevIndex = 1;
-		// }
 		internal_index++;
 		if (internal_index > slides.length) {
 			internal_index= 1;
